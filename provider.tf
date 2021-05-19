f21084eec107ecec91a6ab1bcb5fd6f5816c081a
saikiranbb <76732837+saikiranbb@users.noreply.github.com>
2021-05-19 15:17:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,14 +2,17 @@ provider "google" {
   project     = "jenkins-314106"
   #credentials = "/home/bollabathini/tf-cicd/jenkins-project.json"
 }
+
+/*
 provider "google" {
   version = "~> 3.3.0"
 }
 
 provider "null" {
   version = "~> 2.1"
-}
-/*
+}*/
+
+  /*
 terraform {
   backend "gcs" {
     bucket      = "tf-state-blackmercury-new"
