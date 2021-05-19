39e19d5691becfab630fd9e96f30b69beae2df3e
saikiranbb <76732837+saikiranbb@users.noreply.github.com>
2021-05-19 15:16:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,13 @@ provider "google" {
   project     = "jenkins-314106"
   #credentials = "/home/bollabathini/tf-cicd/jenkins-project.json"
 }
+provider "google" {
+  version = "~> 3.3.0"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
 /*
 terraform {
   backend "gcs" {
